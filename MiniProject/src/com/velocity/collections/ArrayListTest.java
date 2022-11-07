690aadb30e7c412a5698b14b2a7471c8cc098880
unknown <shwetachougule99@gmail.com>
2022-11-07 15:00:18 +0530

    First Commit

---
+++ MiniProject/src/com/velocity/collections/ArrayListTest.java
@@ -0,0 +1,20 @@
+package com.velocity.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("maths");
+		list.add("english");
+		
+		list.add("biology");
+		
+		list.add("physics");
+		
+		System.out.println("list of subjects "+list);
+		
+	}
+
+}
